7feed971047aff86f374c289b1094346fd0eded3
KevinED11 <mendez_kael@hotmail.com>
2023-03-27 11:43:57 -0700

    finish

---
+++ main.py
@@ -0,0 +1,14 @@
+import pyqrcode
+
+if __name__ == "__main__":
+    link_qrcode: list[str] = ["https://www.twitch.tv/",
+                              "https://www.python.org/"
+                              ]
+
+    for number, link in enumerate(link_qrcode):
+        filename_svg = f"qrcode_{number + 1}_" + ".svg"
+        filename_png = f"qrcode_{number + 1}_" + ".png"
+
+        image_qrcode = pyqrcode.create(content=link)
+        image_qrcode.svg(file=filename_svg, scale=8)
+        image_qrcode.png(file=filename_png, scale=10)
